6c688fc6b74f5db09cbaa7a1becf6e1944545b5c
TheEnumbin <enumbin@gmail.com>
2025-01-08 00:52:56 +0600

    Let and var block scope checking

--- ES6_Practice/script.js
+++ ES6_Practice/script.js
@@ -1,16 +1,51 @@
 // Example of hoisting: Wont give error if declared later. Show undefined
+console.log("************ Var variable *********")
 console.log("Before declaration : ")
 console.log(var_variable)
 
 var var_variable = 1
-function check_let() {
+function check_var() {
     var_variable = 2
+    var var_variable_2 = 3
+    console.log("Block scoped variable 2")
+    console.log(var_variable_2)
 }
 
 console.log("Before calling function : ")
 console.log(var_variable)
 
-check_let();
+// Below two lines will show reference error since block scoped
+// console.log("Variable 2 initialized in function : ")
+// console.log(var_variable_2)
+
+check_var();
 
 console.log("After calling function : ")
 console.log(var_variable)
+
+// ****************************************************
+
+console.log("************ Let variable *********")
+console.log("Before declaration : ")
+
+// console.log(let_variable) // Let is hoisted but will show reference error because it is not initialized
+
+let let_variable = 1
+function check_let() {
+    let_variable = 2
+    let let_variable_2 = 3
+    console.log("Block scoped variable 2")
+    console.log(let_variable_2)
+}
+
+// Below two lines will show reference error since block scoped
+// console.log("Variable 2 initialized in function (block scoped) : ")
+// console.log(let_variable_2)
+
+console.log("Before calling function : ")
+console.log(let_variable)
+
+check_let();
+
+console.log("After calling function : ")
+console.log(let_variable)
