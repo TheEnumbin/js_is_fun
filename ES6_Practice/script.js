33c5b0f8beb973c73a45905222c95bc9ab079e9f
TheEnumbin <enumbin@gmail.com>
2025-01-07 23:52:07 +0600

    woring with let

--- ES6_Practice/script.js
+++ ES6_Practice/script.js
@@ -1,1 +1,9 @@
-console.log("hello")
+let letvar = 1
+function check_let() {
+    letvar = 2
+}
+console.log("Before calling function : ")
+console.log(letvar)
+check_let();
+console.log("After calling function : ")
+console.log(letvar)
