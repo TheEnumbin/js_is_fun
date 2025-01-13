8c755b6a187d46bb966861b45cb21ea41cf0be32
TheEnumbin <enumbin@gmail.com>
2025-01-13 23:56:30 +0600

    Complex expression in string

--- ES6_Practice/template_literals.js
+++ ES6_Practice/template_literals.js
@@ -2,7 +2,7 @@ console.log(`*************************************************
 Template Literals Lesson Start
 `);
 
-// Multiline string
+// Multiline string and String interpolation
 console.log("*** Multiline string ***")
 
 var greeting = "Good Morning!!!"
@@ -11,9 +11,16 @@ person.age = 25
 
 let multiline_string = `Hello ${person.firstname} ${person.lastname}
 ${greeting}`
-
 console.log(multiline_string)
 
+console.log("*** Complex expression in string ***")
+person.debt = 50
+person.paid = 20
+
+multiline_string += `
+I think you need to pay me ${person.debt - person.paid} more this week` // Summing integers in string.
+
+console.log(multiline_string)
 
 console.log(` 
 Template Literals End
