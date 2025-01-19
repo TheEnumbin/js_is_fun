84fc7be3761fd923c4c63ab48fa984dd70b169e4
TheEnumbin <enumbin@gmail.com>
2025-01-20 01:04:21 +0600

    Basic different with traditional function

--- ES6_Practice/functions.js
+++ ES6_Practice/functions.js
@@ -1,1 +1,18 @@
-// Example of hoisting: Wont give error if declared later. Show undefined
+console.log(`*************************************************
+Arrow Functions Lesson Start
+`);
+    
+// Basic Difference with traditional function
+console.log("*** Basic Difference ***")
+
+// Traditional function
+console.log(`* Tradiitonal function output *`)
+function addTraditional(a, b) {
+    return a + b;
+}
+console.log(addTraditional(10, 10))
+  
+// Arrow function
+console.log(`* Arrow function output *`)
+const addArrow = (a, b) => a + b;
+console.log(addArrow(5, 5))
