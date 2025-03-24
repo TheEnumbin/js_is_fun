28646aa15aa1c3eb8db8263d0eee6a2565870ace
TheEnumbin <enumbin@gmail.com>
2025-03-24 22:45:06 +0600

    Destructing skip and rest

--- ES6_Practice/destructiring.js
+++ ES6_Practice/destructiring.js
@@ -12,3 +12,25 @@ const [a, b, c] = destructuringArray;
 console.log(a);
 console.log(b);
 console.log(c);
+
+
+// Basic usage
+console.log("*** Skipping Element ***")
+
+const [d,,f] = [1, 2, 3]
+
+console.log(d)
+console.log(f)
+
+// With Rest Operation
+console.log("*** With Rest Operation ***")
+
+const longDestructingarr = ["Enam", "Hassan", "25", "Engineer", "Dhaka"]
+const [firstName, lastname, ...othersAsRest] = longDestructingarr
+
+console.log("Fist Name and Last Name as First elemnts  : ")
+console.log(firstName + ' ' + lastname)
+
+console.log("Others elements by rest  : ")
+console.log(othersAsRest)
+
