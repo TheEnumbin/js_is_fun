3a050a7a803e18687d0843c3b7e5ed232827f5fc
TheEnumbin <66176129+TheEnumbin@users.noreply.github.com>
2025-05-02 23:22:43 +0600

    Update rest_spread.js

--- ES6_Practice/rest_spread.js
+++ ES6_Practice/rest_spread.js
@@ -3,3 +3,9 @@ function sum(...numbers) {
 }
 
 console.log(sum(1, 2, 3, 4)); // Output: 10
+
+
+const [first, ...rest] = [10, 20, 30, 40];
+
+console.log(first); // Output: 10
+console.log(rest);  // Output: [20, 30, 40]
