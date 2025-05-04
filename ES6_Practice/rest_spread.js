1d8b5fd89bed78da323cb44dd77c3a54d8d3ae86
TheEnumbin <enumbin@gmail.com>
2025-05-04 23:01:43 +0600

    update spread rest

--- ES6_Practice/rest_spread.js
+++ ES6_Practice/rest_spread.js
@@ -9,3 +9,20 @@ const [first, ...rest] = [10, 20, 30, 40];
 
 console.log(first); // Output: 10
 console.log(rest);  // Output: [20, 30, 40]
+
+
+const users = [
+  { id: 1, name: "A", email: "a@example.com" },
+  { id: 2, name: "B", email: "b@example.com" }
+];
+
+for (const { id, ...rest } of users) {
+  console.log(id);   // 1, 2
+  console.log(rest); // { name: ..., email: ... }
+}
+
+const obj1 = { a: 1, b: 2 };
+const obj2 = { b: 99, c: 3 };
+const merged = { ...obj1, ...obj2 };
+
+console.log(merged); // { a: 1, b: 99, c: 3 }
