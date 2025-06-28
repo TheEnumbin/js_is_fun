8c6f738acef09b3cf43dd61ee9240aa0f4477e96
TheEnumbin <enumbin@gmail.com>
2025-06-28 23:52:30 +0600

    Added more object literals

--- ES6_Practice/object_literaqls.js
+++ ES6_Practice/object_literaqls.js
@@ -1,2 +1,20 @@
 const name = 'Alice';
 const user = { name }; // same as { name: name }
+
+
+const a = { x: 1 };
+const b = { y: 2 };
+const c = { ...a, ...b }; // { x: 1, y: 2 }
+
+
+const parent = {
+  greet() {
+    return 'Hi';
+  }
+};
+const child = {
+  greet() {
+    return super.greet() + ', there!';
+  }
+};
+Object.setPrototypeOf(child, parent);
